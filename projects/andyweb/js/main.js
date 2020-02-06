b66c9859affd71b27a083318f79a6845fcb9b563
lezzzzz <bratu.david@gmail.com>
2020-02-06 19:01:46 +0200

    v1

--- projects/andyweb/js/main.js
+++ projects/andyweb/js/main.js
@@ -3,14 +3,30 @@ $('#modal1').on('hidden.bs.modal', function (e) {
     // do something...
     $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
   });
-  $('#modal6').on('hidden.bs.modal', function (e) {
+  $('#modal2-1').on('hidden.bs.modal', function (e) {
     // do something...
-    $('#modal6 iframe').attr("src", $("#modal6 iframe").attr("src"));
+    $('#modal2-1 iframe').attr("src", $("#modal2-1 iframe").attr("src"));
+  });
+  $('#modal2-2').on('hidden.bs.modal', function (e) {
+    // do something...
+    $('#modal2-2 iframe').attr("src", $("#modal2-2 iframe").attr("src"));
+  });
+  $('#modal2-3').on('hidden.bs.modal', function (e) {
+    // do something...
+    $('#modal2-3 iframe').attr("src", $("#modal2-3 iframe").attr("src"));
+  });
+  $('#modal2-4').on('hidden.bs.modal', function (e) {
+    // do something...
+    $('#modal2-4 iframe').attr("src", $("#modal2-4 iframe").attr("src"));
+  });
+  $('#modal3-1').on('hidden.bs.modal', function (e) {
+    // do something...
+    $('#modal3-1 iframe').attr("src", $("#modal3-1 iframe").attr("src"));
   });
   
-  $('#modal4').on('hidden.bs.modal', function (e) {
+  $('#modal4-1').on('hidden.bs.modal', function (e) {
     // do something...
-    $('#modal4 iframe').attr("src", $("#modal4 iframe").attr("src"));
+    $('#modal4-1 iframe').attr("src", $("#modal4-1 iframe").attr("src"));
   });
 
 
